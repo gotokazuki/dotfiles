1135af71f170f31bd9c02c41c4e15cadc2c984df
gotokazuki <gotokazuki@pm.me>
2025-10-07 16:59:48 +0900

    udpdate function

--- files/zsh/function.zsh
+++ files/zsh/function.zsh
@@ -7,10 +7,11 @@ zle -N _search_history
 bindkey '^r' _search_history
 
 # search repo
-function _ghq_fzf() {
+function ghqui() {
   ghq_root=$(ghq root)
   cd $(ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'" | xargs -I{} echo $ghq_root/{})
   zle reset-prompt
 }
-zle -N _ghq_fzf
-bindkey '^]' _ghq_fzf
+zle -N ghqui
+bindkey '^]' ghqui
+alias ghqui='ghqui'
