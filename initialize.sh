84a40aa7f4b1788fb5e0e3e8cb9f61ad0db49177
gotokazuki <gotokazuki@pm.me>
2025-10-08 14:50:42 +0900

    fix commands

--- initialize.sh
+++ initialize.sh
@@ -8,7 +8,7 @@ printf '\033[32m%s\033[m\n' 'setup is started...'
 /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/gotokazuki/dotfiles/main/scripts/nvim/setup.sh)"
 /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/gotokazuki/dotfiles/main/scripts/git/setup.sh)"
 /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/gotokazuki/dotfiles/main/scripts/wezterm/setup.sh)"
-source <(curl -fsSL https://raw.githubusercontent.com/gotokazuki/dotfiles/main/scripts/zsh/setup.sh)
+/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/gotokazuki/dotfiles/main/scripts/zsh/setup.sh)"
 /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/gotokazuki/dotfiles/main/scripts/starship/setup.sh)"
 
 printf '\033[32m%s\033[m\n' 'setup is completed'
