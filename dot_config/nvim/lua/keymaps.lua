a575dbcaa89d15b7808980d2b7e1006e07d18789
gotokazuki <gotokazuki@pm.me>
2026-03-01 22:17:37 +0900

    update nvim conf

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 
 local map = vim.keymap.set
-map("n", "<leader>q", ":q<CR>")
-map("n", "<leader>w", ":w<CR>")
-map("n", "<leader>h", ":nohlsearch<CR>")
+local opts = { noremap = true, silent = true }
+map("n", "<leader>q", ":q<CR>", opts)
+map("n", "<leader>w", ":w<CR>", opts)
+map("n", "<leader>h", ":nohlsearch<CR>", opts)
+map("n", "<C-a>", "<Nop>", opts)
+map("n", "<C-x>", "<Nop>", opts)
